a88954ff96123d91a3bf19fdf18c566875c1cb6b
Mattmjarvis <Matt-m-Jarvis@hotmail.com>
2017-10-28 20:29:35 +0100

    More additions to the upgrade menu.
    Upgrade menu button functionality working.
    Need to make it work with currency
    Need to change stats on weapons when upgrade has been clicked

--- Project-Decay/Assets/_Scripts/Managers/UpgradeManager/UpgradeManager.cs
+++ Project-Decay/Assets/_Scripts/Managers/UpgradeManager/UpgradeManager.cs
@@ -6,12 +6,10 @@ using UnityEngine.UI;
 public class UpgradeManager : MonoBehaviour {
 
     public GameObject upgradeInterface;
+    public GameObject[] upgradeMenus;
+
     private InputManager inputManger;
 
-   
-    public WeaponStats ARStats;
-    public WeaponStats SGStats;
-    public WeaponStats PSStats;
 
     // WEAPON SELECTION
     // 0 = ASSAULT RIFLE
@@ -20,7 +18,7 @@ public class UpgradeManager : MonoBehaviour {
     // 3 = SPECIAL
     // 4 = ARMOR
     public Image[] weaponSelection;
-   
+
     // THESE ARE BUTTONS ONLY
     public Image[] Tier1;
     public Image[] Tier2;
@@ -31,7 +29,11 @@ public class UpgradeManager : MonoBehaviour {
     private Image currentlySelected;
     private Image previouslySelected;
 
+    // Text properties
     public Text WeaponName;
+    public Text ArmyDudeText;
+    int timesClicked = 0;
+
     //Colours for weaponselection
     private Color WeaponSelectedColor = new Color(255, 0, 0);
     private Color WeaponUnselectedColor = new Color(255, 255, 255);
@@ -65,6 +67,18 @@ public class UpgradeManager : MonoBehaviour {
         // Apply name of weapon
         WeaponName.text = "Assault Rifle";
 
+        // Enables the correct upgrade menu
+        for (int i = 0; i < upgradeMenus.Length; i++)
+        {
+            if (i == 0)
+            {
+                upgradeMenus[i].SetActive(true);
+            }
+            else
+            {
+                upgradeMenus[i].SetActive(false);
+            }
+        }
     }
 
     // Changes to the upgrades for SG
@@ -80,9 +94,22 @@ public class UpgradeManager : MonoBehaviour {
 
         // Apply name of weapon
         WeaponName.text = "Shotgun";
+
+        // Enables the correct upgrade menu
+        for (int i = 0; i < upgradeMenus.Length; i++)
+        {
+            if (i == 1)
+            {
+                upgradeMenus[i].SetActive(true);
+            }
+            else
+            {
+                upgradeMenus[i].SetActive(false);
+            }
+        }
     }
 
-    // Changes to upgrades for PS
+    // Changes to the upgrades for PS
     public void ClickUpgradePS()
     {
         // Reset colours
@@ -95,24 +122,51 @@ public class UpgradeManager : MonoBehaviour {
 
         // Apply name of weapon
         WeaponName.text = "Pistol";
-    }
 
-    public void SetUpgradeBoxes()
-    {
-        // UPGRADE DB
-            // WEAPON SELECTION
-            // 0 = ASSAULT RIFLE
-            // 1 = SHOTGUN
-            // 2 = PISTOL
-            // 3 = SPECIAL
-            // 4 = ARMOR
-
-        if(currentlySelected = weaponSelection[0])
+        // Enables the correct upgrade menu
+        for (int i = 0; i < upgradeMenus.Length; i++)
         {
-
+            if (i == 2)
+            {
+                upgradeMenus[i].SetActive(true);
+            }
+            else
+            {
+                upgradeMenus[i].SetActive(false);
+            }
         }
     }
 
+    // Changes the speecbubble of the army guy when the bubble is clicked
+    public void ClickArmyDudeSpeech()
+    {
+        timesClicked++;
 
+        if (timesClicked == 1)
+        {
+            ArmyDudeText.text = "Welcome to the Upgrade O Matic, Soldier!";
+        }
+
+        else if (timesClicked == 2)
+        {
+            ArmyDudeText.text = "Pick a weapon, then pick an Upgrade!";
+        }
+
+        else if (timesClicked == 3)
+        {
+            ArmyDudeText.text = "Tip: There's only one upgrade per tier!";
+        }
+
+        else if (timesClicked == 4)
+        {
+            ArmyDudeText.text = "Well? What are you waitin' for?";
+        }
+
+        else if (timesClicked == 5)
+        {
+            ArmyDudeText.text = "Welcome to the Upgrade O Matic, Soldier!";
+            timesClicked = 1;
+        }
+    }
 
 }
---
+++ Project-Decay/Assets/_Scripts/Managers/UpgradeManager/WeaponUpgrades.cs
@@ -0,0 +1,194 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WeaponUpgrades : MonoBehaviour {
+
+    public WeaponStats weapon;
+
+    public Image[] tier1Buttons;
+    public Image[] tier2Buttons;
+    public Image[] tier3Buttons;
+    public Image[] tier4Buttons;
+
+    Color redFade = new Color(255, 0, 0);
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    // TIER UPGRADES
+    // 0 = MAGAZINE SIZE
+    // 1 = RELOAD SPEED
+    // 2 = WEAPON DAMAGE
+
+    #region Tier1 Upgrades
+    // Upgrades the Magazine size
+    public void UpgradeT1Size()
+    {
+        // Sets colours of buttons
+        tier1Buttons[0].color = Color.green;
+        tier1Buttons[1].color = redFade;
+        tier1Buttons[2].color = redFade;
+        // Disables buttons
+        tier1Buttons[0].gameObject.GetComponent<Button>().enabled = false;
+        tier1Buttons[1].gameObject.GetComponent<Button>().enabled = false;
+        tier1Buttons[2].gameObject.GetComponent<Button>().enabled = false;
+    }
+
+    // Upgrades the Reload speed
+    public void UpgradeT1Speed()
+    {
+        // Sets colours of buttons
+        tier1Buttons[0].color = redFade;
+        tier1Buttons[1].color = Color.green;
+        tier1Buttons[2].color = redFade;
+        // Disables buttons
+        tier1Buttons[0].gameObject.GetComponent<Button>().enabled = false;
+        tier1Buttons[1].gameObject.GetComponent<Button>().enabled = false;
+        tier1Buttons[2].gameObject.GetComponent<Button>().enabled = false;
+    }
+
+    // Upgrades the weapon damage
+    public void UpgradeT1Damage()
+    {
+        // Sets colours of buttons
+        tier1Buttons[0].color = redFade;
+        tier1Buttons[1].color = redFade;
+        tier1Buttons[2].color = Color.green;
+        // Disables buttons
+        tier1Buttons[0].gameObject.GetComponent<Button>().enabled = false;
+        tier1Buttons[1].gameObject.GetComponent<Button>().enabled = false;
+        tier1Buttons[2].gameObject.GetComponent<Button>().enabled = false;
+    }
+    #endregion
+
+    #region Tier 2 Upgrades
+    // Upgrades the Magazine size
+    public void UpgradeT2Size()
+    {
+        // Sets colours of buttons
+        tier2Buttons[0].color = Color.green;
+        tier2Buttons[1].color = redFade;
+        tier2Buttons[2].color = redFade;
+        // Disables buttons
+        tier2Buttons[0].gameObject.GetComponent<Button>().enabled = false;
+        tier2Buttons[1].gameObject.GetComponent<Button>().enabled = false;
+        tier2Buttons[2].gameObject.GetComponent<Button>().enabled = false;
+    }
+
+    // Upgrades the Reload speed
+    public void UpgradeT2Speed()
+    {
+        // Sets colours of buttons
+        tier2Buttons[0].color = redFade;
+        tier2Buttons[1].color = Color.green;
+        tier2Buttons[2].color = redFade;
+        // Disables buttons
+        tier2Buttons[0].gameObject.GetComponent<Button>().enabled = false;
+        tier2Buttons[1].gameObject.GetComponent<Button>().enabled = false;
+        tier2Buttons[2].gameObject.GetComponent<Button>().enabled = false;
+    }
+
+    // Upgrades the weapon damage
+    public void UpgradeT2Damage()
+    {
+        // Sets colours of buttons
+        tier2Buttons[0].color = redFade;
+        tier2Buttons[1].color = redFade;
+        tier2Buttons[2].color = Color.green;
+        // Disables buttons
+        tier2Buttons[0].gameObject.GetComponent<Button>().enabled = false;
+        tier2Buttons[1].gameObject.GetComponent<Button>().enabled = false;
+        tier2Buttons[2].gameObject.GetComponent<Button>().enabled = false;
+    }
+    #endregion
+
+    #region Tier 3 Upgrades
+    // Upgrades the Magazine size
+    public void UpgradeT3Size()
+    {
+        // Sets colours of buttons
+        tier3Buttons[0].color = Color.green;
+        tier3Buttons[1].color = redFade;
+        tier3Buttons[2].color = redFade;
+        // Disables buttons
+        tier3Buttons[0].gameObject.GetComponent<Button>().enabled = false;
+        tier3Buttons[1].gameObject.GetComponent<Button>().enabled = false;
+        tier3Buttons[2].gameObject.GetComponent<Button>().enabled = false;
+    }
+
+    // Upgrades the Reload speed
+    public void UpgradeT3Speed()
+    {
+        // Sets colours of buttons
+        tier3Buttons[0].color = redFade;
+        tier3Buttons[1].color = Color.green;
+        tier3Buttons[2].color = redFade;
+        // Disables buttons
+        tier3Buttons[0].gameObject.GetComponent<Button>().enabled = false;
+        tier3Buttons[1].gameObject.GetComponent<Button>().enabled = false;
+        tier3Buttons[2].gameObject.GetComponent<Button>().enabled = false;
+    }
+
+    // Upgrades the weapon damage
+    public void UpgradeT3Damage()
+    {
+        // Sets colours of buttons
+        tier3Buttons[0].color = redFade;
+        tier3Buttons[1].color = redFade;
+        tier3Buttons[2].color = Color.green;
+        // Disables buttons
+        tier3Buttons[0].gameObject.GetComponent<Button>().enabled = false;
+        tier3Buttons[1].gameObject.GetComponent<Button>().enabled = false;
+        tier3Buttons[2].gameObject.GetComponent<Button>().enabled = false;
+    }
+    #endregion
+
+    #region Tier 4 Upgrades
+    // Upgrades the Magazine size
+    public void UpgradeT4Size()
+    {
+        // Sets colours of buttons
+        tier4Buttons[0].color = Color.green;
+        tier4Buttons[1].color = redFade;
+        tier4Buttons[2].color = redFade;
+        // Disables buttons
+        tier4Buttons[0].gameObject.GetComponent<Button>().enabled = false;
+        tier4Buttons[1].gameObject.GetComponent<Button>().enabled = false;
+        tier4Buttons[2].gameObject.GetComponent<Button>().enabled = false;
+    }
+
+    // Upgrades the Reload speed
+    public void UpgradeT4Speed()
+    {
+        // Sets colours of buttons
+        tier4Buttons[0].color = redFade;
+        tier4Buttons[1].color = Color.green;
+        tier4Buttons[2].color = redFade;
+        // Disables buttons
+        tier4Buttons[0].gameObject.GetComponent<Button>().enabled = false;
+        tier4Buttons[1].gameObject.GetComponent<Button>().enabled = false;
+        tier4Buttons[2].gameObject.GetComponent<Button>().enabled = false;
+    }
+
+    // Upgrades the weapon damage
+    public void UpgradeT4Damage()
+    {
+        // Sets colours of buttons
+        tier4Buttons[0].color = redFade;
+        tier4Buttons[1].color = redFade;
+        tier4Buttons[2].color = Color.green;
+        // Disables buttons
+        tier4Buttons[0].gameObject.GetComponent<Button>().enabled = false;
+        tier4Buttons[1].gameObject.GetComponent<Button>().enabled = false;
+        tier4Buttons[2].gameObject.GetComponent<Button>().enabled = false;
+    }
+    #endregion
+}
