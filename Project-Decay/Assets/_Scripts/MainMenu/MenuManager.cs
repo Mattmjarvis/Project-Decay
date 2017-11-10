28b739006fdb664cbc7849e282de5ee13d488825
Damiano_Schirinzi <Damiano16@hotmail.co.uk>
2017-11-10 13:34:23 +0000

    MainMenu added with temp frame.

---
+++ Project-Decay/Assets/_Scripts/MainMenu/MenuManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    public void newGame()
+    {
+        SceneManager.LoadScene("01");
+    }
+    public void ContinueGame()
+    {
+        //Continue game code, load last save etc.
+    }
+    public void Options()
+    {
+        //Options menu code
+    }
+    public void Exit()
+    {
+        Application.Quit();
+    } 
+
+}
