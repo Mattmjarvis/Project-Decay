f62f18fe23dfd2fea3bf3fa81cfd74613496ad04
Mattmjarvis <matt-m-jarvis@hotmail.com>
2017-11-29 15:35:04 +0000

    Changes and Cleaning
    Cleaned up the folder structures. Prefabs are now with the appropriate models in the model folder.
    
    Narrative scene text changed, also changed bgm for narrative scene and added a blurred beach background.

--- Project-Decay/Assets/_Scripts/UI/FadeManager.cs
+++ Project-Decay/Assets/_Scripts/UI/FadeManager.cs
@@ -60,7 +60,7 @@ public class FadeManager : MonoBehaviour {
 
         if (fadeBlackImageOut.color.a >= 1f)
         {
-            SceneManager.LoadScene("01");
+            SceneManager.LoadScene(2);
         }
 
     }
