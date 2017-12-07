2a224cc5607564348f6bd9ce094ecdd7f28b4915
DamianoSchirinzi1 <32909889+DamianoSchirinzi1@users.noreply.github.com>
2017-12-07 23:39:10 +0000

    CleanseZone implemented in main scene
    Small error with health bar when cleanse zone is active

---
+++ Project-Decay/Assets/_Scripts/Managers/CleanseZones/CleanseZoneTrigger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CleanseZoneTrigger : MonoBehaviour {
+
+    public GameObject cleanseZonePrefab;
+
+    void Start()
+    {
+        if (cleanseZonePrefab == null)
+        {
+            Debug.LogError("No cleanse Zone detected");
+        }
+    }
+	void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            cleanseZonePrefab.SetActive(true);
+        }
+    }
+}
