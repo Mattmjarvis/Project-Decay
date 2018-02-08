f144409e9e5e856b3e7ccf3ee3f7caaa84c8a07b
DamianoSchirinzi <Damiano16@hotmail.co.uk>
2018-02-08 20:39:35 +0000

    WeatherManager Script added
    Weateher can now turn on and off.
    Worked on AI
    Will make a smooth transition bettween light changes.

---
+++ Project-Decay/Assets/_Scripts/Managers/Weather/WeatherManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeatherManager : MonoBehaviour {
+
+    public GameObject weather;
+    public GameObject dayLights;
+    public GameObject nightLights;   
+    
+    void Start()
+    {
+        //weather = GameObject.FindGameObjectWithTag("Weather");
+        //nightLights = GameObject.FindGameObjectWithTag("NightLights");
+        //dayLights = GameObject.FindGameObjectWithTag("DayLights");
+    }
+
+    void activateStormSystem()
+    {
+        weather.SetActive(true);
+        nightLights.SetActive(true);
+        dayLights.SetActive(false);       
+    }
+
+    void disableStormSystem()
+    {
+        dayLights.SetActive(true);
+        weather.SetActive(false);
+        nightLights.SetActive(false);
+    }
+
+    //void Update()
+    //{
+    //    if (Input.GetKeyDown(KeyCode.UpArrow))
+    //    {
+    //        activateStormSystem();
+    //    }
+
+    //    if (Input.GetKeyDown(KeyCode.DownArrow))
+    //    {
+    //        disableStormSystem();
+    //    }
+    //}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            activateStormSystem();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            disableStormSystem();
+        }
+    }
+
+}
