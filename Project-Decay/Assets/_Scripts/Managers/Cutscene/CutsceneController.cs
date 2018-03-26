369ba7133093823bf0e12d5c6c39243013bdcc7b
Mattmjarvis <Matt-m-Jarvis@hotmail.com>
2018-03-26 09:56:04 +0100

    Cutscene improved
    Added text which dissapears/ reappears during cutscene (This prompts user to press Space bar to continue)

--- Project-Decay/Assets/_Scripts/Managers/Cutscene/CutsceneController.cs
+++ Project-Decay/Assets/_Scripts/Managers/Cutscene/CutsceneController.cs
@@ -1,18 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class CutsceneController : MonoBehaviour {
 
+    // Get all images and game objects
     public Sprite[] cutsceneImages;
     public GameObject currentImage;
-    public int nextImage = 1;
+    public GameObject continueText;
+    public int nextImage = 1; // Set the next image
 
-    public bool changing = false;
+    public bool changing = false; // Bool to check if the image is already changing
 
-    FadeManager fader;
+    FadeManager fader; // Fade manager component
 
+    // Find the fade manager and alert it to cutscene
     public void Awake()
     {   
         fader = FindObjectOfType<FadeManager>();
@@ -21,10 +25,11 @@ public class CutsceneController : MonoBehaviour {
 
     public void Update()
     {
+        // Moves to next point in cutscene if the spacebar is pressed
         if (Input.GetKeyDown(KeyCode.Space) && changing == false)
         {
-            changing = true;
-            NextImage();
+            changing = true; // Alert to change
+            NextImage(); // Begin changing image coroutine
         }
     }
 
@@ -34,29 +39,34 @@ public class CutsceneController : MonoBehaviour {
         StartCoroutine(NextImageInCutscene());
     }
 
+    // Numerator changes image if there are more available, fades in/out each image
         IEnumerator NextImageInCutscene()
         {
+        // Checks for more images
         if (nextImage <= cutsceneImages.Length - 1)
         {
-            fader.SceneFadeOutBlack();
+            continueText.SetActive(false); // Turns text off
+            fader.SceneFadeOutBlack(); // Begin fadeout
 
             yield return new WaitForSeconds(0.8f);
-            currentImage.GetComponent<SpriteRenderer>().sprite = cutsceneImages[nextImage];
+            // After 0.8s (black screen) the image will change and fade scene back in
+            currentImage.GetComponent<Image>().sprite = cutsceneImages[nextImage];
             
+            fader.SceneFadeInBlack(); // Begin fade in
+            nextImage++; // Increments image counter
 
-            Debug.Log("Fade in");
-            fader.SceneFadeInBlack();
-            nextImage++;
             yield return new WaitForSeconds(2f);
+            // After 2 seconds set changing to false and show text
+            continueText.SetActive(true);
             changing = false;
         }
+
+        // If the final image has been reached then we fade out and change to game scene
         else
         {
             fader.cutscene = false;
             fader.SceneFadeOutBlack();
-
-            Debug.Log("load scene");
-            //SceneManager.LoadScene(3);
+            SceneManager.LoadScene(2);
         }
     
     }
