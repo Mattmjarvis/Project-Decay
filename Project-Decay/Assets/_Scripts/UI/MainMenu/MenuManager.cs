5112aaa015804d43b9d78f0ff90ae4b5c42f4b87
MattJarvis <matt-m-jarvis@hotmail.com>
2018-04-12 14:01:23 +0100

    Damiano- UNITY CRASHED.
    All the work me and gareth did are now dead, we are very upset. Working on in tonight

--- Project-Decay/Assets/_Scripts/UI/MainMenu/MenuManager.cs
+++ Project-Decay/Assets/_Scripts/UI/MainMenu/MenuManager.cs
@@ -3,8 +3,15 @@ using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
 
-public class MenuManager : MonoBehaviour
-{
+public class MenuManager : MonoBehaviour { 
+
+    public GameObject menuButtons;
+
+    public void Start()
+    {
+        StartCoroutine(MoveButtons());
+    }
+
     public void newGame()
     {
         SceneManager.LoadScene(1);
@@ -20,6 +27,28 @@ public class MenuManager : MonoBehaviour
     public void Exit()
     {
         Application.Quit();
-    } 
+    }
+
+    public IEnumerator MoveButtons()
+    {
+        for(; ;)
+        {
+
+            if(menuButtons.transform.localPosition.x >= 0)
+            {
+                Debug.Log("menuButtons.transform.localPosition.x = " + menuButtons.transform.localPosition.x);
+                yield break;
+            }
+            else
+            {
+                menuButtons.transform.localPosition = new Vector3(menuButtons.transform.localPosition.x + 20f, menuButtons.transform.localPosition.y, menuButtons.transform.localPosition.z);
+
+            }
+
+            yield return new WaitForSeconds(0.005f);
+        }
+
+        //yield return null;
+    }
 
 }
