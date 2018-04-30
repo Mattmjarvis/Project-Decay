c98c349031c1d8dd586199eee769ede683cd5931
DamianoSchirinzi <Damiano16@hotmail.co.uk>
2018-04-30 18:31:58 +0100

    Added enemies to base and an interactable note with pin

---
+++ Project-Decay/Assets/_Scripts/Managers/NoteInteract.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteInteract : MonoBehaviour {
+
+    public GameObject interactPrompt;
+    public GameObject noteDisplay;
+
+    InputManager inputManager;
+
+    private void Start()
+    {
+        inputManager = FindObjectOfType<InputManager>();
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(gameObject.tag == "Player")
+        {
+            interactPrompt.SetActive(true);
+
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                noteDisplay.SetActive(true);
+                inputManager.PauseGameplay();
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (gameObject.tag == "Player")
+        {
+            interactPrompt.SetActive(false);
+
+            
+        }
+    }
+}
